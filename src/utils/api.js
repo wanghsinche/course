63a41a5463a587cd28cb0fb475d6e5305d0aa2aa
wanghsinche@hotmail.com <wanghsinche@hotmail.com>
2018-02-08 23:38:48 +0800

    加入详情页面

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
 import data from './mock';
-export const fetchCourseList = async function(page){
+export const fetchCourseList = async function(page) {
     return Promise.resolve(data.courseList);
+}
+export const getDetail = async function(id) {
+	return Promise.resolve(data.detail);
 }
